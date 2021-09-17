ee7033909a37b726503a5d4edb8a1d67ea1975d5
mli <menghua.li@westjet.com>
2021-09-17 13:49:00 -0400

    init

---
+++ src/test/java/com/aloha/learn/spring/ws/rest/RestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aloha.learn.spring.ws.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
