dbe0f55850943f6282da7db4292dc03256e8e4a2
mli <menghua.li@westjet.com>
2021-10-16 20:04:52 -0400

    added API document

--- src/main/java/com/aloha/learn/spring/ws/rest/user/UserResource.java
+++ src/main/java/com/aloha/learn/spring/ws/rest/user/UserResource.java
@@ -21,22 +21,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 /**
  * UserResource, Sample of REST contoller
  */
+@Tag(name = "user", description = "the user API")
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
     private UserDao userDao;
 
+    @Operation(summary = "Retrieve all users", tags = { "user" })
     @GetMapping
     public List<User> retrieveAllUsers() {
         return userDao.findAll();
     }
 
+    @Operation(summary = "Retrieve an user by ID", tags = { "user" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = User.class)),
+                    @Content(mediaType = "application/xml", schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid user ID supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content) })
     @GetMapping("/{id}")
-    public EntityModel<User> retrieveUser(@PathVariable("id") Integer id) {
+    public EntityModel<User> retrieveUser(
+            @Parameter(description = "user ID", required = true) @PathVariable("id") Integer id) {
         User user = userDao.findOne(id);
         if (user == null)
             throw new UserNotFoundException("id=" + id);
